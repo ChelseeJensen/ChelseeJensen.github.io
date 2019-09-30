f07037d3e29537c5dc04ed5a203299a599bbdc22
ChelseeJensen <chelsee.jensen@gmail.com>
2019-09-30 16:24:20 -0600

    Update

---
+++ lesson-2/dates-2.js
@@ -0,0 +1,4 @@
+document.getElementById("lastUpdated").innerHTML = document.lastUpdated;
+
+var copyright_year = new Date();
+document.getElementById("copyright_year").innerHTML = copyright_year.getFullYear();
