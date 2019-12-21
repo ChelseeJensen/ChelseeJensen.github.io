0c20134d9592418082218b9f2deaa41a495fa915
Brad Jensen <bradjensen@MacBook-Pro.local>
2019-12-20 22:31:32 -0700

    final project

---
+++ finalproject/js/webfont.js
@@ -0,0 +1,12 @@
+WebFont.load({
+    google: {
+      families: [
+         'Barlow', 'Barlow Condensed'
+      ]
+    }
+  });
+
+  /* Toggle Menu Function*/
+function toggleMenu() {
+  document.getElementsByClassName("nav")[0].classList.toggle("responsive");
+}
